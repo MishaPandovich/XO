a4cb8f45242c581c14f02a444c17434dec11b6fd
Виктория <Ovcharka@gmail.com>
2019-06-10 13:02:12 +0300

    разделение кода на три блока: модель, вид, контроллер

--- src/index.js
+++ src/index.js
@@ -2,21 +2,68 @@ class Model {
 	constructor() {
 		this.dataX = [];
 		this.dataO = [];
-		this.player = '';
+		this.player = 'X';
 		this.stepCount = 0;
+		this.windCombinations = [
+	  	[1,2,3],
+	  	[1,4,7],
+	  	[1,5,9],
+	  	[2,5,8],
+	  	[3,6,9],
+	  	[3,5,7],
+	  	[4,5,6],
+	  	[7,8,9]
+		];
+
+		this.ceil = document.querySelectorAll('.game-item'); // потом удалить
 	}
 
 	addStep(ceil, num) {
-		this.player === 'X' ? this.dataX.push(num) : this.dataO.push(num);
-
 		if (!ceil.textContent) {
+			this.player === 'X' ? this.dataX.push(num) : this.dataO.push(num);
 			this.stepCount++;
-			this.changePlayer(ceil);
+			ceil.style.pointerEvents = 'none';
+		}
+
+		/*переписать этот блок ~ попробовать решить проблему с помощью паттерна наблюдатель*/
+		if ((this.dataX.length > 2 || this.dataO.length > 2) && (this.checkWin(this.dataX, num) || this.checkWin(this.dataO, num))) {
+			for (var i = 0; i < this.ceil.length; i++) {
+				this.ceil[i].style.pointerEvents = 'none';
+			}
+
+			return true;
 		}
 	}
 
-	changePlayer(ceil) {
+	checkWin(dataArr, num) {
+		for (var i = 0; i < this.windCombinations.length; i++) {
+			var someWinArr = this.windCombinations[i];
+			var count = 0;
+
+			if (someWinArr.indexOf(num) !== -1) {
+				for (var j = 0; j < someWinArr.length; j++) {
+					if (dataArr.indexOf(someWinArr[j]) !== -1) {
+						count++;
+						if (count === 3) {
+							return true;
+						}
+					}
+				}
+			}
+
+			count = 0;
+		}
+	}
+
+	changePlayer() {
 		this.player === 'X' ? (this.player = 'O') : (this.player = 'X');
+	}
+
+	reset() {
+		this.dataX = [];
+	  this.dataO = [];
+		this.player = 'X';
+	  this.stepCount = 0;
 	}
 }
 
@@ -25,16 +72,29 @@ class View {
 		this.gameTitle = document.querySelector('#message');
 	}
 
-	fillCeil(ceil, player) {	
+	fillCeil(ceil, player, stepCount) {	
 		if (!ceil.textContent) {
 			ceil.innerText = player;
+			this.changeGameTitle(stepCount, player);
 		}
 	}
 
+	showWin(player) {
+		var gameTitle;
+		(player === 'X')  ? (gameTitle = 'O') : (gameTitle = 'X');
+		this.gameTitle.innerText = 'Победил игрок: ' + gameTitle;
+	}
+
+	// сделать более универальным
 	changeGameTitle(stepCount, player) {
-		var titlePlayer;
-		(player === 'X') ? (titlePlayer = 'O') : (titlePlayer = 'X');
-		(stepCount === 9) ? (message.innerText = 'НИЧЬЯ') : (message.innerText = 'Ходит игрок: ' + titlePlayer);
+		var gameTitle;
+		(player === 'X')  ? (gameTitle = 'O') : (gameTitle = 'X');
+		(stepCount === 9) ? (message.innerText = 'НИЧЬЯ') : (message.innerText = 'Ходит игрок: ' + gameTitle);
+	}
+
+	// избавиться
+	setInintState(player) {
+		message.innerText = 'Ходит игрок: ' + player;
 	}
 }
 
@@ -43,17 +103,32 @@ class Controller {
 		var model = new Model();
 		var view = new View();
 		this.ceil = document.querySelectorAll('.game-item');
+		this.buttonReset = document.querySelector('#reset-game');
 
 		for (var i = 0; i < this.ceil.length; i++) {
-			ceilClickHandler(this.ceil[i]);
+			this.ceil[i].addEventListener('click', ceilClickHandler);
 		}
 
-		function ceilClickHandler(ceil) {
-			ceil.addEventListener('click', function() {
-				model.addStep(ceil, +ceil.getAttribute('data-ceil'));
-				view.fillCeil(ceil, model.player);
-				view.changeGameTitle(model.stepCount, model.player);
-			})
+		function ceilClickHandler() {
+			var result = model.addStep(this, +this.getAttribute('data-ceil'));
+			view.fillCeil(this, model.player, model.stepCount);
+			model.changePlayer(this);
+
+			if (result) {
+				view.showWin(model.player);
+			}
+		}
+
+		this.buttonReset.addEventListener('click', reset);
+
+		function reset() {
+			for (var i=0; i < controller.ceil.length; i++) {
+				controller.ceil[i].innerText = '';
+				controller.ceil[i].style.pointerEvents = 'auto';
+			}
+
+			model.reset();
+			view.setInintState(model.player);
 		}
 	}
 }
@@ -66,5 +141,9 @@ var controller = new Controller;
 // придумать как передавать элемент на который нажали ceil[i] +
 // отрефакторить код + 
 // добавить счётчик игроков + 
-// при каждом нажатии счётчик увеличивается и проверяется + 
+// при каждом нажатии счётчик увеличивается и проверяется + (наблюд)
 // сделать так, чтобы повторно нельзя было нажимать на кнопку +
+// помле нажатия надо удалить спососбность нажатия + 
+// начинать делать проверку на выйгрыш +
+// наблюдатель при выйгрыше должно приходить оповещение контроллеру (обновление) -
+// написать очистку игры+
